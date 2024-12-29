e1d2cdff8935566273c0b5f059011a2f798a9f22
gabreucast <gabreucast@gmail.com>
2024-12-28 21:46:19 -0300

    adjustar liquidez_diaria.xml

--- app/src/main/java/com/gabreucast/desafiofinal1/MainActivity.kt
+++ app/src/main/java/com/gabreucast/desafiofinal1/MainActivity.kt
@@ -13,8 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_main)) { v, insets ->
+        setContentView(R.layout.liquidez_diaria)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.liquidez_diaria)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
