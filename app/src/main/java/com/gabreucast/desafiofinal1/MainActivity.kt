b933e33933610ddd13428ce13444198fa2194ce8
gabreucast <gabreucast@gmail.com>
2025-01-18 21:46:38 -0300

    adicionar a 1ra pagina ou tela inicial do projeto e finalizar o projeto

--- app/src/main/java/com/gabreucast/desafiofinal1/MainActivity.kt
+++ app/src/main/java/com/gabreucast/desafiofinal1/MainActivity.kt
@@ -13,8 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.liquidez_diaria)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.liquidez_diaria)) { v, insets ->
+        setContentView(R.layout.tela_inicial)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tela_inicial)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
